d995713021c568e0b69d6dced87b609e6b0f5f66
Md-Asikur <asikurrahaman997@gamil.com>
2023-08-12 18:17:16 +0600

    hi

--- digitic-admin/src/features/auth/authServices.js
+++ digitic-admin/src/features/auth/authServices.js
@@ -3,7 +3,7 @@ import { config } from "../../utils/axiosconfig";
 import { base_url } from "../../utils/baseUrl";
 const login = async (user) => {
   const response = await axios.post(`${base_url}user/admin-login`, user, {
-    withCredentials:"include"
+    withCredentials:true
   });
   if (response.data) {
     localStorage.setItem("user", JSON.stringify(response.data));
