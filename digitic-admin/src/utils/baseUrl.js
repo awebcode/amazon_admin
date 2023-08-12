ed12a291a6878d75d36106a4320ad56011d4bb50
Md-Asikur <asikurrahaman997@gamil.com>
2023-08-12 19:11:14 +0600

    good

--- digitic-admin/src/utils/baseUrl.js
+++ digitic-admin/src/utils/baseUrl.js
@@ -1,1 +1,2 @@
-export const base_url = "http://localhost:5000/api/"; //https://amazonadmin-backend.onrender.com
+export const base_url = "https://amazonadmin-backend.onrender.com/api/"; //https://amazonadmin-backend.onrender.com
+// http://localhost:5000/api/
