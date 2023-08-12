32e1dc7389c29c85a29641a16cf85ed07b09bfd2
Md-Asikur <asikurrahaman997@gamil.com>
2023-08-12 16:00:43 +0600

    amazon admin

---
+++ digitic-backend/index.js
@@ -0,0 +1,52 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const dbConnect = require("./config/dbConnect");
+const { notFound, errorHandler } = require("./middlewares/errorHandler");
+const app = express();
+const dotenv = require("dotenv").config();
+
+const PORT =process.env.PORT;
+const authRouter = require("./routes/authRoute");
+const productRouter = require("./routes/productRoute");
+const blogRouter = require("./routes/blogRoute");
+const categoryRouter = require("./routes/prodcategoryRoute");
+const blogcategoryRouter = require("./routes/blogCatRoute");
+const brandRouter = require("./routes/brandRoute");
+const colorRouter = require("./routes/colorRoute");
+const enqRouter = require("./routes/enqRoute");
+const couponRouter = require("./routes/couponRoute");
+const uploadRouter = require("./routes/uploadRoute");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
+const path=require("path")
+dbConnect();
+app.use(morgan("dev"));
+//  app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use("/api/user", authRouter);
+app.use("/api/product", productRouter);
+app.use("/api/blog", blogRouter);
+app.use("/api/category", categoryRouter);
+app.use("/api/blogcategory", blogcategoryRouter);
+app.use("/api/brand", brandRouter);
+app.use("/api/coupon", couponRouter);
+app.use("/api/color", colorRouter);
+app.use("/api/enquiry", enqRouter);
+app.use("/api/upload", uploadRouter);
+app.use(fileUpload())
+app.use(notFound);
+app.use(errorHandler);
+app.use(express.static(path.join(__dirname, "../digitic-admin/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../digitic-admin/build/index.html"));
+});
+app.listen(PORT, () => {
+  console.log(`Server is running  at PORT ${PORT}`);
+});
